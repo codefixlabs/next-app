bfb7da552c090d330d7c5d85b9a5da767b8b6736
Vuong Phan <13298232+thevuong@users.noreply.github.com>
2024-03-12 18:42:46 +0700

    Update Playwright configuration to use environment variables for port and base URL

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,6 +6,9 @@ import { defineConfig, devices } from "@playwright/test";
  */
 // require('dotenv').config();
 
+const PORT = process.env.PORT ?? 3000;
+const BASE_URL = process.env.BASE_URL ?? `http://localhost:${PORT}`;
+
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
@@ -24,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://127.0.0.1:3000",
+    baseURL: BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -69,9 +72,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "pnpm run build && pnpm run start",
+    url: BASE_URL,
+    reuseExistingServer: !process.env.CI,
+  },
 });
